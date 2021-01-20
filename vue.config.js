d74bf0556e01a3977bfd18ab1430881d928f6c67
paul0127 <paul@eso.com.tw>
2021-01-20 18:16:46 +0800

    0120

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    chainWebpack : config=>{
+        config.module.rule('eslint').use('eslint-loader').loader('eslint-loader').tap(options=>{
+            options.fix = true
+            return options
+        })
+    },
+    baseUrl : '/ddr'
+}
