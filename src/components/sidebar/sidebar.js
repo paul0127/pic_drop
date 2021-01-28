7f75d181c2d2b68a2f13241ad500ca48cccc3760
paul0127 <paul@eso.com.tw>
2021-01-28 23:23:47 +0800

    0128

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -28,9 +28,28 @@ export default {
         this.$store.dispatch('rect/setBackground', { object: object })
     },
     add_rect(val){
-      let object = this.rects[val]
-      console.log(object)
-
+      let url = this.rects[val].imgUrl
+      let img = new Image()
+      img.src = url;
+        
+      let object ={
+        zoom:1,
+        x: 0,
+        y: 0,
+        width: img.width,
+        height: img.height,
+        rotation: 0,
+        minHeight: 10,
+        minWidth: 10,
+        rotatable: true,
+        resizable: true,
+        draggable: true,
+        acceptRatio: false,
+        active: false,
+        zIndex:1,
+        imgUrl:url,
+      }
+      
       this.$store.dispatch('rect/addRect', { object: object })
     }
   }
